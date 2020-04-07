0a674c6e85345e99459ec2590318c6417873093c
Sebastian D <jsduenass@unal.edu.co>
2020-04-07 18:03:13 -0500

    implementation of Update and Delete functions

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ clear= lambda: os.system('cls')
 clients=''
 clear()
 
+def _get_client_name():
+    return input('Enter the client name: ')
+
 def create_client():
-    client_name= input('Enter the client name: ') 
+    client_name= _get_client_name() 
     new_client(client_name)
 
     print('{} was succesfully created'.format(client_name))
@@ -16,9 +19,25 @@ def new_client(client_name):
     clients+=client_name+', '
 
 def delete_client():
-    client_name= input('Enter the client name: ') 
+    global clients
+    client_name= _get_client_name() 
+    if client_name in clients:
+        clients=clients.replace(client_name+', ', '')
+        print('{} was succesfully deleted'.format(client_name))
+    else:
+        print('{} was not found on client list '.format(client_name))
+    list_clients()
 
-    print('{} was succesfully deleted'.format(client_name))
+def update_client():
+    global clients
+    list_clients()
+    client_name= _get_client_name() 
+    if client_name in clients:
+        updated_client_name= input('{} was found, enter it\'s new name: '.format(client_name))
+        clients=clients.replace(client_name, updated_client_name)
+    else:
+        print('{} was not found on client list unable to update'.format(client_name))
+    list_clients()
 
 def list_clients():
     global clients
@@ -31,18 +50,22 @@ def not_valid():
     input('COMMAND NOT VALID PRESS ENTER AND TRY AGAIN')
 
 def select(argument):
-    switcher = { 'c': 'create_client', 'd': 'delete_client', 'l':'list_clients', 'q':'end'}
+    switcher = { 'C': 'create_client', 'D': 'delete_client','L':'list_clients', 'U':'update_client', 'Q':'end'}
     func = switcher.get(argument, "not_valid")
     return   func+'()'
 
 
 if __name__ == '__main__':
+   
     welcome_f=open('welcome_message.txt','r')
     welcome_banner=welcome_f.read()
-    
+    input_file=open('list.txt','r+')
+    clients=input_file.read() 
+    main_menu= { 'C': 'create_client', 'D': 'delete_client','L':'list_clients', 'U':'update_client', 'Q':'end'}
     
     while True:
         command= str(input(welcome_banner))
+        command=command.upper()     # account for lower case
         clear()
         func=select(command)
         if func=='end()':
