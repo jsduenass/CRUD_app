8ba7bccc7a18034d30330015948670d3bec3d2f2
Sebastian D <jsduenass@unal.edu.co>
2020-04-07 22:03:19 -0500

    change of clients data type from string to list and it's necesary addaptation in the functions

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ clear= lambda: os.system('cls')
 clients=''
 clear()
 
+
 def _get_client_name():
     return input('Enter the client name: ')
 
@@ -16,14 +17,14 @@ def create_client():
 def new_client(client_name):
     """ Este es un texto de ayuda para esta funcion """
     global clients
-    clients+=client_name+', '
+    clients.append(client_name)
 
 def delete_client():
     global clients
     list_clients()
     client_name= _get_client_name() 
     if client_name in clients:
-        clients=clients.replace(client_name+', ', '')
+        clients.remove(client_name)
         print('{} was succesfully deleted'.format(client_name))
     else:
         print('{} was not found on client list '.format(client_name))
@@ -34,23 +35,27 @@ def update_client():
     list_clients()
     client_name= _get_client_name() 
     if client_name in clients:
+        index= clients.index(client_name)
         updated_client_name= input('{} was found, enter it\'s new name: '.format(client_name))
-        clients=clients.replace(client_name, updated_client_name)
+        clients[index]= updated_client_name
     else:
         print('{} was not found on client list unable to update'.format(client_name))
     list_clients()
 
 def list_clients():
     global clients
-    lista= clients.split(', ')
-    print(clients)
-    return lista
+    
+    for idx ,client in enumerate(clients):
+        print('{}: {}'.format(idx,client)) 
+    
+
 def save():
     global input_file
     global clients
     input_file.truncate(0)
     input_file.seek(0)
-    input_file.write(clients)
+    formated_client_list=', '.join(clients)
+    input_file.write(formated_client_list)
 
 def not_valid():
     input('COMMAND NOT VALID PRESS ENTER AND TRY AGAIN')
@@ -75,8 +80,8 @@ if __name__ == '__main__':
     welcome_banner=welcome_f.read()
 
     input_file=open('list.txt','r+')
-    clients=input_file.read() 
-    
+    input_text=input_file.read() 
+    clients=input_text.split(', ')
     main_menu= { 'C': 'create_client', 'D': 'delete_client','L':'list_clients', 'U':'update_client','S': 'save','Q':'end'}
     yes_no={'Y':'save' , 'N': 'end'}
     while True:
@@ -105,6 +110,7 @@ if __name__ == '__main__':
 
 # Notas 
 # funcion dir() y help()  permite conocer todas la funciones que puede hacer una variable
+# [inicio:final:paso]
 # git location
 # E/Documentos/codigo/platzi/python/CRUD_app
 # git log --stat
@@ -117,7 +123,7 @@ if __name__ == '__main__':
 # git fetch
 # git merge
 # git pull = fetch + merge
-# rama experimental
+# branch experiment  rama experimental
 # git switch experiment
 
 
